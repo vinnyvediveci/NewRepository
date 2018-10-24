10c1e12c4d932e1ba20d74d84a34c66f810df717
vinnyvediveci <vndwitharanage@gmail.com>
2018-10-24 16:59:36 +0100

    First day

--- Battleships.py
+++ Battleships.py
@@ -10,22 +10,22 @@ class BattleShipGame:
                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
 
-    def add(self, ship, x1, y1, changeTo):
+    def add(self, ship, x1, y1, x2, y2, changeTo):
         assert isinstance(ship, Ship)
         BattleShipGame.battleField[x1][y1] = changeTo
-        BattleShipGame.battleField[x1 +1][y1] = changeTo
-        BattleShipGame.battleField[x1 +2][y1] = changeTo
+        BattleShipGame.battleField[x1+1][y1] = changeTo
+        BattleShipGame.battleField[x1+2][y1] = changeTo
 
-        #if x1 == x2:
-          #y1 = BattleShipGame.battleField[y1]
-          #y2 = BattleShipGame.battleField[y2]
-          #for x in range(y1, y2):
-            # x = 1
-       # elif y1 == y2:
-          #x1 = BattleShipGame.battleField[x1]
-          #x2 = BattleShipGame.battleField[x2]
-          #for y in range(x1, x2):
-            # y = 1
+        if x1 == x2:
+          y1 = BattleShipGame.battleField[y1]
+          y2 = BattleShipGame.battleField[y2]
+          for x in range(y1, y2):
+            x = 1
+        elif y1 == y2:
+          x1 = BattleShipGame.battleField[x1]
+          x2 = BattleShipGame.battleField[x2]
+          for y in range(x1, x2):
+            y = 1
 
 
 
@@ -67,7 +67,7 @@ class Carrier(Ship):
         Ship.__init__(self,  5)
 
 battleShipGame = BattleShipGame()
-battleShipGame.add(Battleship(), 0, 2, 1)
+battleShipGame.add(Battleship(), 0, 2, 1, 2, 5)
 
 #
 
